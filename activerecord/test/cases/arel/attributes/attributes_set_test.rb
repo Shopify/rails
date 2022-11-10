df55d20b18be8ded8941ae4422de530e8c6f0e1b
Nikita Vasilevsky <nikita.vasilevsky@shopify.com>
2022-11-10 23:02:38 +0000

    [Active Record] Support row-constructor clauses for PostgreSQL/MySQL

---
+++ activerecord/test/cases/arel/attributes/attributes_set_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "../helper"
+require "ostruct"
+
+module Arel
+  module Attributes
+    class AttributesSetTest < Arel::Spec
+      # anything?
+    end
+  end
+end
