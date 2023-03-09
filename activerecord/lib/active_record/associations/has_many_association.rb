5cd45145d416f92514b2ae48cc2d8fbea2c778f2
Paarth Madan <paarth.madan@shopify.com>
2023-03-09 17:11:04 -0500

    Use composite key in has_many destroy_async

--- activerecord/lib/active_record/associations/has_many_association.rb
+++ activerecord/lib/active_record/associations/has_many_association.rb
@@ -33,10 +33,10 @@ module ActiveRecord
 
           unless target.empty?
             association_class = target.first.class
-            primary_key_column = association_class.primary_key.to_sym
+            primary_key_column = (association_class.query_constraints_list || Array(association_class.primary_key)).map(&:to_sym)
 
             ids = target.collect do |assoc|
-              assoc.public_send(primary_key_column)
+              primary_key_column.map { |col| assoc.public_send(col) }
             end
 
             ids.each_slice(owner.class.destroy_association_async_batch_size || ids.size) do |ids_batch|
