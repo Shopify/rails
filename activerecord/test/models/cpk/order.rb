4f33b3770d990a2d21961b074c30ab4e7b704823
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2023-05-31 00:01:24 -0500

    Add composite primary key validity check to associations.
    Raise ActiveRecord::CompositePrimaryKeyMismatchError when a belongs_to,
    has_one, or has_many foreign key and primary key don't have the same length.

--- activerecord/test/models/cpk/order.rb
+++ activerecord/test/models/cpk/order.rb
@@ -8,6 +8,11 @@ module Cpk
     alias_attribute :id_value, :id
 
     has_many :order_agreements, primary_key: :id
+    has_many :books, query_constraints: [:shop_id, :order_id]
+  end
+
+  class BrokenOrder < Order
     has_many :books
+    has_one :book
   end
 end
