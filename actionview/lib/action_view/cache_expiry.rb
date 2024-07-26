89bc6167ca7a12a8199e8208c610a5a611458e7a
Étienne Barrié <etienne.barrie@gmail.com>
2024-07-26 16:11:49 +0200

    Make ActionView::CacheExpiry::ViewReloader#build_watcher public

--- actionview/lib/action_view/cache_expiry.rb
+++ actionview/lib/action_view/cache_expiry.rb
@@ -2,7 +2,7 @@
 
 module ActionView
   module CacheExpiry # :nodoc: all
-    class ViewReloader
+    class ViewReloader # :nodoc:
       def initialize(watcher:, &block)
         @mutex = Mutex.new
         @watcher_class = watcher
@@ -29,27 +29,27 @@ module ActionView
         watcher.execute
       end
 
+      def build_watcher
+        @mutex.synchronize do
+          old_watcher = @watcher
+
+          if @watched_dirs != dirs_to_watch
+            @watched_dirs = dirs_to_watch
+            new_watcher = @watcher_class.new([], @watched_dirs) do
+              reload!
+            end
+            @watcher = new_watcher
+
+            # We must check the old watcher after initializing the new one to
+            # ensure we don't miss any events
+            @previous_change ||= old_watcher&.updated?
+          end
+        end
+      end
+
       private
         def reload!
           ActionView::LookupContext::DetailsKey.clear
-        end
-
-        def build_watcher
-          @mutex.synchronize do
-            old_watcher = @watcher
-
-            if @watched_dirs != dirs_to_watch
-              @watched_dirs = dirs_to_watch
-              new_watcher = @watcher_class.new([], @watched_dirs) do
-                reload!
-              end
-              @watcher = new_watcher
-
-              # We must check the old watcher after initializing the new one to
-              # ensure we don't miss any events
-              @previous_change ||= old_watcher&.updated?
-            end
-          end
         end
 
         def rebuild_watcher
