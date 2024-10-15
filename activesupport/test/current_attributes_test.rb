adb53e2070169fb734f564952bbf45f40d42aca7
Jean Boussier <jean.boussier@gmail.com>
2024-10-15 16:19:08 +0200

    [BUG] Stack consistency error (sp: 142, bp: 141)
    ```
    cd activesupport
    bin/test test/current_attributes_test.rb --seed 23757
    ```

--- activesupport/test/current_attributes_test.rb
+++ activesupport/test/current_attributes_test.rb
@@ -81,83 +81,6 @@ class CurrentAttributesTest < ActiveSupport::TestCase
 
   setup { assert_nil Session.previous, "Expected Session to not have leaked state" }
 
-  test "read and write attribute" do
-    Current.world = "world/1"
-    assert_equal "world/1", Current.world
-  end
-
-  test "read overwritten attribute method" do
-    Current.request = "request/1"
-    assert_equal "request/1 something", Current.request
-  end
-
-  test "set attribute via overwritten method" do
-    Current.account = "account/1"
-    assert_equal "account/1", Current.account
-    assert_equal "account/1's person", Current.person.name
-  end
-
-  test "set auxiliary class via overwritten method" do
-    Current.person = Person.new(42, "David", "Central Time (US & Canada)")
-    assert_equal "Central Time (US & Canada)", Time.zone.name
-    assert_equal 42, Session.current
-  end
-
-  test "resets auxiliary classes via callback" do
-    Current.person = Person.new(42, "David", "Central Time (US & Canada)")
-    assert_equal "Central Time (US & Canada)", Time.zone.name
-
-    Current.reset
-    assert_equal "UTC", Time.zone.name
-    assert_equal 42, Session.previous
-    assert_nil Session.current
-  end
-
-  test "set auxiliary class based on current attributes via before callback" do
-    Current.person = Person.new(42, "David", "Central Time (US & Canada)")
-    assert_nil Session.previous
-    assert_equal 42, Session.current
-
-    Current.reset
-    assert_equal 42, Session.previous
-    assert_nil Session.current
-  end
-
-  test "set attribute only via scope" do
-    Current.world = "world/1"
-
-    Current.set(world: "world/2") do
-      assert_equal "world/2", Current.world
-    end
-
-    assert_equal "world/1", Current.world
-  end
-
-  test "set multiple attributes" do
-    Current.world = "world/1"
-    Current.account = "account/1"
-
-    Current.set(world: "world/2", account: "account/2") do
-      assert_equal "world/2", Current.world
-      assert_equal "account/2", Current.account
-    end
-
-    assert_equal "world/1", Current.world
-    assert_equal "account/1", Current.account
-  end
-
-  test "using keyword arguments" do
-    Current.set_world_and_account(world: "world/1", account: "account/1")
-
-    assert_equal "world/1", Current.world
-    assert_equal "account/1", Current.account
-
-    hash = {}
-    assert_same hash, Current.get_world_and_account(hash)
-    assert_equal "world/1", hash[:world]
-    assert_equal "account/1", hash[:account]
-  end
-
   setup { @testing_teardown = false }
   teardown { assert_equal 42, Session.current if @testing_teardown }
 
@@ -176,10 +99,6 @@ class CurrentAttributesTest < ActiveSupport::TestCase
     Current.person = Person.new(42, "David", "Central Time (US & Canada)")
     assert_equal "David, in Central Time (US & Canada)", Current.intro
     assert_equal "David, in Central Time (US & Canada)", Current.instance.intro
-  end
-
-  test "respond_to? for methods that have not been called" do
-    assert_equal true, Current.respond_to?("respond_to_test")
   end
 
   test "CurrentAttributes use fiber-local variables" do
