5b65ca34d94c198e95608cecde2ba2e295f6339f
Adrianna Chang <adrianna.chang@shopify.com>
2025-02-27 12:20:33 -0500

    IsolatedExecutionState with fiber storage

--- activesupport/lib/active_support/isolated_execution_state.rb
+++ activesupport/lib/active_support/isolated_execution_state.rb
@@ -13,16 +13,18 @@ module ActiveSupport
       def isolation_level=(level)
         return if level == @isolation_level
 
-        unless %i(thread fiber).include?(level)
-          raise ArgumentError, "isolation_level must be `:thread` or `:fiber`, got: `#{level.inspect}`"
+        unless %i(thread fiber fiber_storage).include?(level)
+          raise ArgumentError, "isolation_level must be `:thread`, `:fiber`, or `:fiber_storage`, got: `#{level.inspect}`"
         end
+
+        # Should we raise if fiber storage is not supported on the Ruby version?
 
         clear if @isolation_level
 
         @scope =
           case level
           when :thread; Thread
-          when :fiber; Fiber
+          when :fiber, :fiber_storage; Fiber
           end
 
         @isolation_level = level
@@ -33,26 +35,23 @@ module ActiveSupport
       end
 
       def [](key)
-        if state = @scope.current.active_support_execution_state
-          state[key]
-        end
+        state[key]
       end
 
       def []=(key, value)
-        state = (@scope.current.active_support_execution_state ||= {})
         state[key] = value
       end
 
       def key?(key)
-        @scope.current.active_support_execution_state&.key?(key)
+        state.key?(key)
       end
 
       def delete(key)
-        @scope.current.active_support_execution_state&.delete(key)
+        state.delete(key)
       end
 
       def clear
-        @scope.current.active_support_execution_state&.clear
+        state.clear
       end
 
       def context
@@ -65,7 +64,17 @@ module ActiveSupport
         # and streaming should be rethought.
         context.active_support_execution_state = other.active_support_execution_state.dup
       end
-    end
+
+      private
+        def state
+          case isolation_level
+          when :fiber_storage
+            scope[:active_support_execution_state] ||= {}
+          else
+            context.active_support_execution_state ||= {}
+          end
+        end
+      end
 
     self.isolation_level = :thread
   end
