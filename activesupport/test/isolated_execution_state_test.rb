1edab00f6a58505cf2e4d5959b992d44d98b87b5
Adrianna Chang <adrianna.chang@shopify.com>
2025-03-04 17:11:26 -0500

    Implement :fiber_storage isolation level in `ActiveSupport::IsolatedExecutionState`
    This allows us to use `ActiveSupport::IsolatedExecutionState` in scenarios
    where we'd like state to propagate to child Fibers / Threads spawned from within
    the current Fiber scope.

--- activesupport/test/isolated_execution_state_test.rb
+++ activesupport/test/isolated_execution_state_test.rb
@@ -39,6 +39,44 @@ class IsolatedExecutionStateTest < ActiveSupport::TestCase
     assert_nil Thread.new { ActiveSupport::IsolatedExecutionState[:test] }.value
   end
 
+  test "#[] when isolation level is :fiber_storage, state separated between threads" do
+    ActiveSupport::IsolatedExecutionState.isolation_level = :fiber_storage
+
+    Thread.new do
+      ActiveSupport::IsolatedExecutionState[:test] = 42
+      assert_equal 42, ActiveSupport::IsolatedExecutionState[:test]
+    end
+
+    assert_nil ActiveSupport::IsolatedExecutionState[:test]
+  end
+
+  test "#[] when isolation level is :fiber_storage, child fibers / threads inherit state" do
+    ActiveSupport::IsolatedExecutionState.isolation_level = :fiber_storage
+
+    ActiveSupport::IsolatedExecutionState[:test] = 42
+    assert_equal 42, ActiveSupport::IsolatedExecutionState[:test]
+    enumerator = Enumerator.new do |yielder|
+      yielder.yield ActiveSupport::IsolatedExecutionState[:test]
+    end
+
+    assert_equal 42, enumerator.next
+    assert_equal 42, Thread.new { ActiveSupport::IsolatedExecutionState[:test] }.value
+  end
+
+  test "#[] when isolation level is :fiber_storage, child fibers / threads do not mutate parent" do
+    ActiveSupport::IsolatedExecutionState.isolation_level = :fiber_storage
+
+    ActiveSupport::IsolatedExecutionState[:test] = 42
+    assert_equal 42, ActiveSupport::IsolatedExecutionState[:test]
+    enumerator = Enumerator.new do |yielder|
+      ActiveSupport::IsolatedExecutionState[:test] = 99
+      yielder.yield ActiveSupport::IsolatedExecutionState[:test]
+    end
+
+    assert_equal 99, enumerator.next
+    assert_equal 42, ActiveSupport::IsolatedExecutionState[:test]
+  end
+
   test "changing the isolation level clear the old store" do
     original = ActiveSupport::IsolatedExecutionState.isolation_level
     other = ActiveSupport::IsolatedExecutionState.isolation_level == :fiber ? :thread : :fiber
