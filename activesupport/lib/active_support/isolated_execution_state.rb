cc24026f052080ddd7ac1db4dff08326cea5b817
Adrianna Chang <adrianna.chang@shopify.com>
2025-03-04 17:11:26 -0500

    Implement :fiber_storage isolation level in `ActiveSupport::IsolatedExecutionState`
    This allows us to use `ActiveSupport::IsolatedExecutionState` in scenarios
    where we'd like state to propagate to child Fibers / Threads spawned from within
    the current Fiber scope.

--- activesupport/lib/active_support/isolated_execution_state.rb
+++ activesupport/lib/active_support/isolated_execution_state.rb
@@ -13,8 +13,8 @@ module ActiveSupport
       def isolation_level=(level)
         return if level == @isolation_level
 
-        unless %i(thread fiber).include?(level)
-          raise ArgumentError, "isolation_level must be `:thread` or `:fiber`, got: `#{level.inspect}`"
+        unless %i(thread fiber fiber_storage).include?(level)
+          raise ArgumentError, "isolation_level must be `:thread`, `:fiber` or `:fiber_storage`, got: `#{level.inspect}`"
         end
 
         clear if @isolation_level
@@ -22,7 +22,7 @@ module ActiveSupport
         @scope =
           case level
           when :thread; Thread
-          when :fiber; Fiber
+          when :fiber, :fiber_storage; Fiber
           end
 
         @isolation_level = level
@@ -33,26 +33,29 @@ module ActiveSupport
       end
 
       def [](key)
-        if state = @scope.current.active_support_execution_state
-          state[key]
-        end
+        state[key]
       end
 
       def []=(key, value)
-        state = (@scope.current.active_support_execution_state ||= {})
-        state[key] = value
+        if isolation_level == :fiber_storage
+          new_state = state.dup
+          new_state[key] = value
+          scope[:active_support_execution_state] = new_state
+        else
+          state[key] = value
+        end
       end
 
       def key?(key)
-        @scope.current.active_support_execution_state&.key?(key)
+        state.key?(key)
       end
 
       def delete(key)
-        @scope.current.active_support_execution_state&.delete(key)
+        state.delete(key)
       end
 
       def clear
-        @scope.current.active_support_execution_state&.clear
+        state.clear
       end
 
       def context
@@ -65,6 +68,16 @@ module ActiveSupport
         # and streaming should be rethought.
         context.active_support_execution_state = other.active_support_execution_state.dup
       end
+
+      private
+        def state
+          case isolation_level
+          when :fiber_storage
+            scope[:active_support_execution_state] ||= {}
+          else
+            context.active_support_execution_state ||= {}
+          end
+        end
     end
 
     self.isolation_level = :thread
