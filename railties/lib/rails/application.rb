40a62782a6b7b677e477d2b3ea594cdb43840e16
Étienne Barrié <etienne.barrie@gmail.com>
2025-11-12 14:45:49 +0100

    ActiveSupport::Inflector::Inflections

--- railties/lib/rails/application.rb
+++ railties/lib/rails/application.rb
@@ -142,6 +142,7 @@ module Rails
       ActionDispatch::ParamBuilder.freeze
       Mime.freeze
       ActionDispatch::Callbacks.freeze
+      ActiveSupport::Inflector::Inflections.freeze
       Ractor.make_shareable self
     end
 
