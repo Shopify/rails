0ce83554273b2874608efd2e7875d4499eae0b11
Étienne Barrié <etienne.barrie@gmail.com>
2025-11-13 16:28:57 +0100

    ActionView::Base

--- railties/lib/rails/application.rb
+++ railties/lib/rails/application.rb
@@ -151,6 +151,7 @@ module Rails
       ActionDispatch::Response.freeze
       ActionDispatch::Request.ractor_shareable
       ActionView::LookupContext.ractor_shareable
+      ActionView::Base.freeze
       Ractor.make_shareable self
     end
 
