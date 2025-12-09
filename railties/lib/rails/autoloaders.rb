85b441937ebef135a59a3f83e32e0820914d4d52
Étienne Barrié <etienne.barrie@gmail.com>
2025-12-09 16:50:42 +0100

    Avoid freezing Zeitwerk::Loader altogether

--- railties/lib/rails/autoloaders.rb
+++ railties/lib/rails/autoloaders.rb
@@ -18,7 +18,6 @@ module Rails
       #
       # Delaying this `require` up to this point is a convenient trade-off.
       require "zeitwerk"
-      self.class.zeitwerk_loader_freeze
 
       @main = Zeitwerk::Loader.new
       @main.tag = "rails.main"
@@ -29,69 +28,14 @@ module Rails
       @once.inflector = Inflector
     end
 
-    def self.zeitwerk_loader_freeze
-      if [::Zeitwerk::Loader, ::Zeitwerk::Loader::Config, ::Zeitwerk::Cref::Map]
-        .any? { it.instance_methods(false).include?(:freeze) }
-        raise "remove me" # FIXME
-      else
-        ::Zeitwerk::Loader.class_eval do
-          def freeze
-            raise "can't freeze a loader that hasn't been eager loaded" unless @eager_loaded
-
-            @autoloads.freeze
-            @inceptions.freeze
-            @autoloaded_dirs.freeze
-            @to_unload.freeze
-            @namespace_dirs.freeze
-            @shadowed_files.freeze
-            @setup.freeze
-            @eager_loaded.freeze
-
-            @mutex = nil
-            @dirs_autoload_monitor = nil
-
-            super
-          end
-        end
-
-        ::Zeitwerk::Loader::Config.class_eval do
-          def freeze
-            @inflector.freeze
-            @logger.freeze
-            @tag.freeze
-            @initialized_at.freeze
-            @roots.freeze
-            @ignored_glob_patterns.freeze
-            @ignored_paths.freeze
-            @collapse_glob_patterns.freeze
-            @collapse_dirs.freeze
-            @eager_load_exclusions.freeze
-            @reloading_enabled.freeze
-            @on_setup_callbacks.freeze
-            @on_load_callbacks.freeze
-            @on_unload_callbacks.freeze
-
-            super
-          end
-        end
-
-        ::Zeitwerk::Cref::Map.class_eval do
-          def freeze
-            @mutex = nil
-
-            super
-          end
-        end
-      end
-    end
-
     def freeze
-      @main.freeze
-      @once.freeze
+      @main = nil
+      @once = nil
       super
     end
 
     def each
+      return if frozen?
       yield main
       yield once
     end
