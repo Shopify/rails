5a4cbda30c17662c2a4cffc57e135f95e657f5ce
Hartley McGuire <skipkayhil@gmail.com>
2025-12-12 14:07:00 -0500

    Enable models to connect/query different adapters.
    Previously, models cached many adapter specific values which would
    prevent making queries against different database types. Now, those
    caches are per `schema_context`, which can be configured in
    `database.yml`:
    
    ```yaml
    primary:
      adapter: trilogy
      database: myapp_development
    primary_pg:
      adapter: postgresql
      database: myapp_development
      schema_context: pg
    ```
    
    This enables models to connect to both MySQL and PostgreSQL databases in
    the same application, and query them successfully:
    
    ```ruby
    class User < ApplicationRecord
      connects_to shards: {
        mysql: { writing: :primary, reading: :primary },
        pg: { writing: :primary_pg, reading: :primary_pg },
      }
    end
    
    User.connected_to(shard: :mysql) { User.first } # queries MySQL
    User.connected_to(shard: :pg) { User.first } # queries PostgreSQL
    ```

--- activerecord/lib/active_record/database_configurations/hash_config.rb
+++ activerecord/lib/active_record/database_configurations/hash_config.rb
@@ -217,6 +217,10 @@ module ActiveRecord
         configuration_hash.fetch(:dump_schema_migrations, ActiveRecord.dump_schema_migrations)
       end
 
+      def schema_context # :nodoc:
+        configuration_hash.fetch(:schema_context, "default").to_s
+      end
+
       private
         def schema_file_type(format)
           case format.to_sym
