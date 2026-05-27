fffea3431f1a65e9c2dde057120d117b415b155d
Andrew Novoselac <andrew.novoselac@shopify.com>
2026-05-27 19:17:27 -0400

    Detach local cache middleware from cache store on freeze

--- activesupport/lib/active_support/cache/strategy/local_cache_middleware.rb
+++ activesupport/lib/active_support/cache/strategy/local_cache_middleware.rb
@@ -11,13 +11,17 @@ module ActiveSupport
         # This class wraps up local storage for middlewares. Only the middleware method should
         # construct them.
         class Middleware # :nodoc:
-          attr_reader :name
-          attr_accessor :cache
+          attr_reader :name, :cache
 
           def initialize(name, cache)
             @name = name
-            @cache = cache
+            self.cache = cache
             @app = nil
+          end
+
+          def cache=(cache)
+            @cache = cache
+            @local_cache_key = cache.send(:local_cache_key)
           end
 
           def new(app)
@@ -26,18 +30,32 @@ module ActiveSupport
           end
 
           def call(env)
-            cache.new_local_cache
+            new_local_cache
             response = @app.call(env)
             response[2] = ::Rack::BodyProxy.new(response[2]) do
-              cache.unset_local_cache
+              unset_local_cache
             end
             cleanup_on_body_close = true
             response
           rescue Rack::Utils::InvalidParameterError
             [400, {}, []]
           ensure
-            cache.unset_local_cache unless cleanup_on_body_close
+            unset_local_cache unless cleanup_on_body_close
           end
+
+          def freeze
+            @cache = nil
+            super
+          end
+
+          private
+            def new_local_cache
+              LocalCacheRegistry.set_cache_for(@local_cache_key, LocalStore.new)
+            end
+
+            def unset_local_cache
+              LocalCacheRegistry.set_cache_for(@local_cache_key, nil)
+            end
         end
       end
     end
