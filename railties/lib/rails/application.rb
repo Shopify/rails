62fe5dbdde4544ca07c5db0fc2b4804796ffbee8
Andrew Novoselac <andrew.novoselac@shopify.com>
2026-05-27 19:27:09 -0400

    Detach autoloaders when freezing application

--- railties/lib/rails/application.rb
+++ railties/lib/rails/application.rb
@@ -183,6 +183,11 @@ module Rails
       Ractor.make_shareable(self)
     end
 
+    def freeze
+      @autoloaders = nil
+      super
+    end
+
     # Returns true if the application is initialized.
     def initialized?
       @initialized
