7fc9d5e20a2a818622ad252efcd3b4cfca2a5ba8
Edouard CHIN <chin.edouard@gmail.com>
2026-07-02 18:28:24 +0200

    Ractor: dispatch ActiveStorage metadata analysis to the main Ractor
    Image analysis shells out to ruby-vips, which memoizes introspection in a class
    variable (Vips::Introspect@@introspect_cache) and dispatches through GLib -- a
    class variable is categorically unreachable from a non-main Ractor, so vips
    simply can't run there. Instead of reimplementing the C bindings, override
    Blob#extract_metadata_via_analyzer to run on the main Ractor (which owns the
    store and native libs) and return the resulting metadata Hash (shareable
    scalars). This mirrors how DB work is dispatched to the connection-owning main
    Ractor.
    
    Assisted-By: devx/f5ab299d-d944-4ec1-8705-3109a370c696

--- activestorage/lib/active_storage/ractor_patches.rb
+++ activestorage/lib/active_storage/ractor_patches.rb
@@ -21,3 +21,28 @@ require "active_support/ractors"
   next unless ActiveStorage.respond_to?(name)
   ActiveSupport::Ractors.capture_class_reader(ActiveStorage, name)
 end
+
+# Metadata extraction shells out to image libraries (ruby-vips / MiniMagick).
+# ruby-vips in particular caches introspection in a class variable
+# (Vips::Introspect@@introspect_cache) and dispatches through GLib -- neither of
+# which is reachable from a non-main Ractor. Rather than reimplement the C
+# bindings, dispatch the analysis to the main Ractor (which owns the store and
+# the native libraries) and bring back the resulting metadata Hash, which is
+# made of shareable scalars.
+if defined?(ActiveStorage::Blob)
+  module ActiveStorage
+    module RactorAnalyzeDispatch
+      private
+        def extract_metadata_via_analyzer
+          return super if Ractor.main? || !persisted?
+
+          blob_id = id
+          Ractor::Dispatch.main.run do
+            metadata = ActiveStorage::Blob.find(blob_id).send(:extract_metadata_via_analyzer)
+            Ractor.make_shareable(metadata)
+          end
+        end
+    end
+  end
+  ActiveStorage::Blob.prepend(ActiveStorage::RactorAnalyzeDispatch)
+end
