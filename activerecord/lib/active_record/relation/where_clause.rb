8480dece1d62582044ff11ca7a61a3f96a488f21
Étienne Barrié <etienne.barrie@gmail.com>
2026-08-05 19:13:59 +0200

    Revert WhereClause::EMPTY make_shareable!
    Restore the upstream form. Upstream now freezes the constant deeply
    (new([].freeze).freeze), which is equivalent for Ractor purposes.

--- activerecord/lib/active_record/relation/where_clause.rb
+++ activerecord/lib/active_record/relation/where_clause.rb
@@ -200,7 +200,7 @@ module ActiveRecord
           end
         end
 
-        EMPTY = new([]).make_shareable!
+        EMPTY = new([]).freeze
         ARRAY_WITH_EMPTY_STRING = [""].freeze
 
         def non_empty_predicates
